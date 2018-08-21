da7356461f54a9894a28ab0cb04994654d766ee7
wangzhouhu <wangzhouhu@tuandai.com>
2018-08-21 18:17:06 +0800

    asp.net core mvc leaning

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvcApp.Models;
+using System.Net.Http;
+
+namespace mvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+      IHttpClientFactory httpClientFactory;             
+        public IActionResult Index()
+        {
+           // var client=httpClientFactory.CreateClient();
+           // var result=client.GetStringAsync("");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# asp.net Core MVC 应用
+
+## 官方文档
+* aspnet内库源码： [https://github.com/aspnet](https://github.com/aspnet)
+* dotnet系统内库源码：[https://github.com/dotnet](https://github.com/dotnet)
+
+### 1、 自定义端口访问
