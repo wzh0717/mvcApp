93716bce811fd0079560b76e3ddcf2d084906b70
wzh0717 <31395351+wzh0717@users.noreply.github.com>
2018-08-23 11:35:57 +0800

    部署与发布

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ namespace mvcApp
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
@@ -20,14 +19,17 @@ namespace mvcApp
                .AddJsonFile("hosting.json", true)
                .Build();
 
-            BuildWebHost(args, config).Run();
-            //BuildWebHost(args).Run();
+            //BuildWebHost(args, config).Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args, IConfiguration config) =>
+        public static IWebHost BuildWebHost(string[] args /*, IConfiguration config*/) =>
             WebHost.CreateDefaultBuilder(args)
-                //  .UseUrls("http://*:5001", "http://*:5002")
-                .UseConfiguration(config)
+                .UseKestrel()
+                .UseUrls("http://*:5001", "http://*:5002")
+                //  .UseConfiguration(config)
+                //.UseHttpSys()
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,10 @@ namespace mvcApp
         {
             services.AddHttpClient();
             services.AddMvc();
+            services.Configure<IISOptions>(options =>
+            {
+                options.ForwardClientCertificate = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
